8c5c13521b1483b522ea0b3762b34dd7c7fd1d41
Higor Carmanini <higor.c@turing.com>
2022-05-29 21:52:20 -0300

    Improve environment variables

--- cds/database/config.py
+++ cds/database/config.py
@@ -2,14 +2,12 @@ from sqlmodel import Session, create_engine
 
 from cds.settings import ENV_VARS
 
-DATABASE_URI = (
-    'mysql://{user}:{password}@{address}:{port}/{database}'.format(
-        user=ENV_VARS.CDS_MYSQL_USER,
-        password=ENV_VARS.CDS_MYSQL_PASSWORD.get_secret_value(),
-        address=ENV_VARS.CDS_MYSQL_ADDRESS,
-        port=ENV_VARS.CDS_MYSQL_PORT.get_secret_value(),
-        database=ENV_VARS.CDS_MYSQL_DATABASE
-    )
+DATABASE_URI = 'mysql://{user}:{password}@{address}:{port}/{database}'.format(
+    user=ENV_VARS.CDS_MYSQL_USER,
+    password=ENV_VARS.CDS_MYSQL_PASSWORD.get_secret_value(),
+    address=ENV_VARS.CDS_MYSQL_ADDRESS.get_secret_value(),
+    port=ENV_VARS.CDS_MYSQL_PORT.get_secret_value(),
+    database=ENV_VARS.CDS_MYSQL_DATABASE
 )
 
 engine = create_engine(
